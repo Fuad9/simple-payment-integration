89243fef8afbfc2d6b749f3b55c4b457ff3c50c9
Fuad9 <alfuad8548@gmail.com>
2020-12-15 11:29:32 +0600

    done

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-$api_key_id = "rzp_test_mZNzawpicVOX3V"
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,50 +8,23 @@ $api_key_id = "rzp_test_mZNzawpicVOX3V"
 </head>
 
 <body>
-    <button id="rzp-button1">Pay</button>
-    <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
-    <script>
-        var options = {
-            "key": <?php echo $api_key_id ?>, // Enter the Key ID generated from the Dashboard
-            "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-            "currency": "INR",
-            "name": "Acme Corp",
-            "description": "Test Transaction",
-            "image": "https://example.com/your_logo",
-            "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            "handler": function(response) {
-                alert(response.razorpay_payment_id);
-                alert(response.razorpay_order_id);
-                alert(response.razorpay_signature)
-            },
-            "prefill": {
-                "name": "Gaurav Kumar",
-                "email": "gaurav.kumar@example.com",
-                "contact": "9999999999"
-            },
-            "notes": {
-                "address": "Razorpay Corporate Office"
-            },
-            "theme": {
-                "color": "#3399cc"
-            }
-        };
-        var rzp1 = new Razorpay(options);
-        rzp1.on('payment.failed', function(response) {
-            alert(response.error.code);
-            alert(response.error.description);
-            alert(response.error.source);
-            alert(response.error.step);
-            alert(response.error.reason);
-            alert(response.error.metadata.order_id);
-            alert(response.error.metadata.payment_id);
-        });
-        document.getElementById('rzp-button1').onclick = function(e) {
-            rzp1.open();
-            e.preventDefault();
-        }
-    </script>
+
+    <form action="https://www.example.com/payment/success/" method="POST">
+    <script
+        src="https://checkout.razorpay.com/v1/checkout.js"
+        data-key="rzp_test_mZNzawpicVOX3V"
+        data-amount="29935" 
+        data-currency="INR"
+        data-buttontext="Pay with Razorpay"
+        data-name="Acme Corp"
+        data-description="A Wild Sheep Chase is the third novel by Japanese author Haruki Murakami"
+        data-image="https://example.com/your_logo.jpg"
+        data-prefill.name="Gaurav Kumar"
+        data-prefill.email="gaurav.kumar@example.com"
+        data-theme.color="#000000"
+    ></script>
+    <input type="hidden" custom="Hidden Element" name="hidden">
+    </form>
 
 </body>
-
 </html>
